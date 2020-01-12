83bd1be31a9aeac71fa1e5473fb684913116f817
Nicolas Metras <nicolas.metras@gmail.com>
2020-01-11 19:26:28 -0800

    add encryption functions

--- crypto.py
+++ crypto.py
@@ -1,5 +1,22 @@
 from Crypto.Cypher import AES
 from Crypto.Protocol.KDF import PBKDF2
+import os
+
+def encrypt(text, key, key_size = 256):
+    pad = lambda s: s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+    text = pad(text)
+    initialization = Random.new().read(16)
+    cipher = AES.new(key, AES.MODE_CBC, initialization)
+    return initialization + cipher.encrypt(text)
+
+def file_encrypt(file_name, key):
+    with open(file_name, 'rb') as input_file:
+        plaintext = input_file.read()
+    encrypted_text = encrypt(plaintext, key)
+    with open(file_name + ".enc", 'wb') as output_file:
+        output_file.write(encrypted_text)
+    os.remove(file_name)
+
 
 # This function takes an alphanumeric string as a password and passes it to pycryptodome's PBKDF2 
 # algorithm to generate an encryption key 
